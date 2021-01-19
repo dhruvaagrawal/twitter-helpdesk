2ee6045c14f3cb298b4ea1af4fe32a85dc1874f3
dhruvaagrawal <dhruva1998@gmail.com>
2021-01-19 07:29:56 +0530

    First commit

---
+++ app/build/precache-manifest.540cb2fe8ade3b60beb87a48ee253078.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f12bae15822f00f26d8980c3b785d1",
+    "url": "/test_cms/index.html"
+  },
+  {
+    "revision": "180482acebaa9a4bdd13",
+    "url": "/test_cms/static/css/3.7a23d34f.chunk.css"
+  },
+  {
+    "revision": "eb1cda6dbd0a66a4c6eb",
+    "url": "/test_cms/static/css/main.d933d494.chunk.css"
+  },
+  {
+    "revision": "3a17de623f8e3c7f440b",
+    "url": "/test_cms/static/js/0.8b98cd0a.chunk.js"
+  },
+  {
+    "revision": "180482acebaa9a4bdd13",
+    "url": "/test_cms/static/js/3.8617dad2.chunk.js"
+  },
+  {
+    "revision": "7ac8977a71d81efd10b4",
+    "url": "/test_cms/static/js/4.b531b70b.chunk.js"
+  },
+  {
+    "revision": "5f106c7e8dc391ddb8ea",
+    "url": "/test_cms/static/js/5.62276a48.chunk.js"
+  },
+  {
+    "revision": "692a86a7b711aa775f13",
+    "url": "/test_cms/static/js/6.55b97944.chunk.js"
+  },
+  {
+    "revision": "d5bf1c2fb58446e01570",
+    "url": "/test_cms/static/js/7.07005e2d.chunk.js"
+  },
+  {
+    "revision": "eb1cda6dbd0a66a4c6eb",
+    "url": "/test_cms/static/js/main.a22d7031.chunk.js"
+  },
+  {
+    "revision": "6d4f9a41a3e4fb413d09",
+    "url": "/test_cms/static/js/runtime-main.6baa77f1.js"
+  }
+]);
